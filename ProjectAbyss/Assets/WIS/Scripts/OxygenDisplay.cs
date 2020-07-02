bedfe249dc06fbe673dc8fd3b63a9d7e8745c6a5
William <w.schmitt@rubika-edu.com>
2020-07-02 17:01:08 +0200

    Tests UI

---
+++ ProjectAbyss/Assets/WIS/Scripts/OxygenDisplay.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class OxygenDisplay : MonoBehaviour
+{
+
+    public float oxygen = 100f;
+    public Text oxygenText;
+
+    private void Update()
+    {
+
+        oxygenText.text = "Oxygen : " + oxygen + "%";
+
+        
+
+
+    }
+
+
+}
